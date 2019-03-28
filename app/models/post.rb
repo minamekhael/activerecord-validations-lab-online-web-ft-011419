85e85a3a3b3b70212eca0a84439b5d50da7b9ec3
Mina Mekhael <mina.mekhael@ymail.com>
2019-03-28 14:29:33 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, presence:true
+  validates :content, length: {minimum: 150}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "%w(value) is not valid"}
+  validate :clickbait?
+
+   PHRASE = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/
+  ]
+
+    def clickbait?
+    if PHRASE.none? {|c| c.match title}
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
